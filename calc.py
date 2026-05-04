7950694697103f962f90c517ed1b6505583e38cd
PEAnderson-22 <PEAnderson@csi.edu>
2026-05-04 10:38:42 -0600

    I decided to go back to my calc.py and make it so the user can't use negative numbers. I'm not entirely sure if this was necessary since the numbers will be buttons, but I decided to do it anyway.

--- calc.py
+++ calc.py
@@ -1,14 +1,22 @@
 #This is where my formulas for each equation will go. The ValueErrors will also be included here.
 class Calculator:
     def addition(self,a,b):
+        if a <=0 or b<=0: #This is making it so the user can't use negative number, and I will put this on all of them.
+            raise ValueError("Your number cannot be negative.")
         return a+b
     def subtraction(self,a,b):
+        if a <=0 or b<=0:
+            raise ValueError("Your number cannot be negative.")
         return a-b
     def multiply(self,a,b):
+        if a <=0 or b<=0:
+            raise ValueError("Your number cannot be negative.")
         return a*b
     def division(self,a,b):
-        if b==0
-        raise ValueError("Can't divide by 0")#This will make it so the user will know that they can;t do that.
+        if a <=0 or b<=0:
+            raise ValueError("Your number cannot be negative.")
+        elif b == 0:
+            raise ValueError("Can't divide by 0")#This will make it so the user will know that they can;t do that.
         return a/b
 #What I have just done is add in all my formulas. These will take the numbers the user selects and do whichever one they choose.
 
